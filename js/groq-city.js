78742e134acfa6246734255cce7413e5b143f623
Jeseuss <bereanfit@gmail.com>
2026-02-18 00:45:52 -0800

    Update groq-city.js

--- js/groq-city.js
+++ js/groq-city.js
@@ -37,7 +37,7 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 // Groq API Key - FIXED the reference
-const GROQ_API_KEY = "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"; 
+const GROQ_API_KEY = "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"; 
 
 // Game State
 let currentUser = null;
